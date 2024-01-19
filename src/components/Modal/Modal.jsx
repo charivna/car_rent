d05abd8a83dc32835789b781e6390c2418dfc8be
charivna <kate.dzhurinskaya@gmail.com>
2024-01-19 16:51:59 +0100

    finish modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,46 @@
-import { Backdrop, ImageContainer, Info, ModalWindow } from './Modal.styled';
+import { useEffect } from 'react';
+import icons from '../../common/sprite.svg';
+
+import {
+  Accessories,
+  AccessoriesDetail,
+  Backdrop,
+  BtnClose,
+  Condition,
+  Description,
+  FlexWrap,
+  ImageContainer,
+  Info,
+  ModalWindow,
+  Model,
+  Rental,
+  RentalButton,
+  Tittle,
+  Value,
+} from './Modal.styled';
 
 export const Modal = ({ isOpen, onClose, car }) => {
+  useEffect(() => {
+    const handleKeyPress = e => {
+      onClose();
+    };
+
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [onClose]);
   if (isOpen) return null;
+
+  const rentalConditionsString = car.rentalConditions || '';
+
+  const rentalConditionsArray = rentalConditionsString.split('\n');
 
   return (
     <Backdrop onClick={onClose}>
       <ModalWindow onClick={e => e.stopPropagation()}>
         <ImageContainer>
-          {' '}
           <img
             src={car.img}
             alt={car.make}
@@ -15,12 +48,49 @@ export const Modal = ({ isOpen, onClose, car }) => {
           />
         </ImageContainer>
 
-        <h3>{`${car.make} ${car.model} ${car.year}`}</h3>
+        <Tittle>
+          {`${car.make}`}
+          <Model> {`${car.model}`} </Model>
+          {`${car.year}`}
+        </Tittle>
+
         <Info>
           {`${car.address.split(',').slice(-2).join(', ')} | ${
             car.rentalCompany
           } | ${car.type} | ${car.id} | ${car.accessories[0]}`}
         </Info>
+
+        <Description>{`${car.description}`}</Description>
+        <Accessories>Accessories and functionalities:</Accessories>
+
+        <AccessoriesDetail>
+          {car.accessories.map((accessory, index) => (
+            <p key={index}>{accessory} | </p>
+          ))}
+        </AccessoriesDetail>
+        <Rental>Rental Conditions: </Rental>
+        <FlexWrap>
+          {rentalConditionsArray.map((condition, index) => (
+            <Condition key={index}>{condition}</Condition>
+          ))}
+          <Condition>
+            {' '}
+            Millage:&nbsp;{' '}
+            <Value>
+              {car.mileage.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </Value>{' '}
+          </Condition>
+
+          <Condition>
+            Price:&nbsp; <Value> {`${car.rentalPrice}`} </Value>
+          </Condition>
+        </FlexWrap>
+        <RentalButton to="tel:+1234567890">Rental Car</RentalButton>
+        <BtnClose onClick={onClose}>
+          <svg width={12} height={12}>
+            <use href={`${icons}#icon-cross`}></use>
+          </svg>
+        </BtnClose>
       </ModalWindow>
     </Backdrop>
   );
--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Backdrop = styled.div`
@@ -14,9 +15,10 @@ export const Backdrop = styled.div`
 `;
 
 export const ModalWindow = styled.div`
+  position: relative;
   box-sizing: border-box;
   width: 541px;
-  height: 752px;
+
   padding: 15px 35px;
   border-radius: 24px;
   background: #fff;
@@ -30,6 +32,7 @@ export const ImageContainer = styled.div`
   border-radius: 14px;
   border-color: transparent;
   margin-bottom: 14px;
+  margin-top: 25px;
   background: linear-gradient(
       180deg,
       rgba(18, 20, 23, 0.5) 2.5%,
@@ -44,5 +47,115 @@ export const Info = styled.p`
   max-width: 277px;
   font-weight: 400;
   line-height: 1.5;
-  margin-bottom: 28px;
+  margin-bottom: 14px;
+`;
+
+export const Tittle = styled.h3`
+  margin-bottom: 8px;
+  color: #121417;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 1.33;
+`;
+export const Model = styled.span`
+  color: #3470ff;
+`;
+
+export const Description = styled.p`
+  font-size: 14px;
+  line-height: 1.42;
+  width: 461px;
+  margin-bottom: 24px;
+`;
+
+export const Accessories = styled.p`
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.42;
+  margin-bottom: 8px;
+`;
+
+export const AccessoriesDetail = styled.div`
+  color: rgba(18, 20, 23, 0.5);
+  font-size: 12px;
+  max-width: 461px;
+  font-weight: 400;
+  line-height: 1.5;
+  margin-bottom: 24px;
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 24px;
+`;
+
+export const Rental = styled.p`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.42;
+  margin-bottom: 8px;
+`;
+
+export const Condition = styled.p`
+  font-family: Montserrat;
+  color: #363535;
+  font-size: 12px;
+  line-height: 1.5;
+  border-radius: 35px;
+  background: #f9f9f9;
+  display: flex;
+  padding: 7px 14px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FlexWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
+
+export const Value = styled.span`
+  color: #3470ff;
+  font-family: Montserrat;
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 1.5;
+  letter-spacing: -0.24px;
+`;
+
+export const RentalButton = styled(NavLink)`
+  box-sizing: border-box;
+  padding: 12px 50px;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1.4;
+  white-space: nowrap;
+  text-decoration: none;
+  color: #fff;
+  border-radius: 12px;
+  width: 168px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 44px;
+  margin-top: 24px;
+  margin-bottom: 40px;
+  background-color: #3470ff;
+  transition: box-shadow 0.3s ease;
+  cursor: pointer;
+  &:hover {
+    background-color: #0b44cd;
+  }
+`;
+
+export const BtnClose = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 24px;
+  height: 24px;
+  background-color: transparent;
+  position: absolute;
+  top: 16px;
+  right: 16px;
 `;
