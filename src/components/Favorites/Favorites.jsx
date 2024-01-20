e29f7c4181808892f160d5f3b01173df7ee7d0ba
charivna <kate.dzhurinskaya@gmail.com>
2024-01-20 17:27:37 +0100

    favorites style

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,27 +1,26 @@
 // Favorites.js
 import CarCard from 'components/CarCard/CarCard';
 import { useSelector } from 'react-redux';
+import { Container, List } from './Favorites.styled';
 
 const Favorites = () => {
   const likedCars = useSelector(state => state.favorites.likedCars);
   const allCars = useSelector(state => state.catalog.cars);
 
   return (
-    <div>
-      <h2>Улюблені автомобілі</h2>
-      <ul>
+    <Container>
+      <List>
         {likedCars.map(carId => {
           const car = allCars.find(c => c.id === carId);
 
           if (car) {
-            // Видаліть цей <li>, оскільки CarCard вже рендериться в <li>
             return <CarCard key={carId} car={car} />;
           } else {
             return null;
           }
         })}
-      </ul>
-    </div>
+      </List>
+    </Container>
   );
 };
 
---
+++ src/components/Favorites/Favorites.styled.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  box-sizing: border-box;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1440px;
+  padding: 0 115px;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  margin-left: auto;
+  margin-right: auto;
+  gap: 29px;
+  padding: 0;
+`;
